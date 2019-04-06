23a471297753bf8c847956f1bf64824158f1a501
Shrinath <Shrinathbhosale12@gmail.com>
2019-04-06 16:57:23 +0530

    added functionality to remove options

--- public/js/create-poll.js
+++ public/js/create-poll.js
@@ -9,12 +9,11 @@ function createOption(event) {
     const n = ++optionNumber;
 
     const div = document.createElement('div');
-    div.setAttribute('class', 'form-group');
 
     const option = `
-        <div class="form-group">
-            <label for="opt${n}"><span class="label-text">Option ${n}</span></label>
+        <div class="form-group" id="opt${n}-fg">
             <input type="text" name="options" id="opt${n}" class="form-control" placeholder="max 30 characters..." maxlength="30" required>
+            <button class="btn btn-danger" id="opt${n}-rm">&#10008;</button>
         </div>
     `;
 
@@ -24,6 +23,10 @@ function createOption(event) {
     document.getElementById(`opt${n}`).focus();
 
     event.target.scrollIntoView({behavior: "smooth"});
+
+    document.getElementById(`opt${n}-fg`).addEventListener('click', removeOption);
+
+    //console.log(optionNumber);
 }
 
 function validateForm(event) {
@@ -36,4 +39,17 @@ function validateForm(event) {
         document.getElementById('error-msg').innerHTML = '';
         event.target.submit();
     }
+}
+
+function removeOption(event) {
+    event.preventDefault();
+    
+    if(event.target.nodeName !== "BUTTON")
+        return;
+
+    const optId = event.target.id.substring(3).split('-')[0];
+    document.getElementById(`opt${optId}-fg`).remove();
+    --optionNumber;
+
+    //console.log(optionNumber);
 }
